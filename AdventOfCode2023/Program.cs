6792dfc7f74e6eb8ac55e343c3dd045857b365e8
Jan-P. R <janpatrickrotter@gmail.com>
2023-12-02 21:10:33 +0100

    shruggin

--- AdventOfCode2023/Program.cs
+++ AdventOfCode2023/Program.cs
@@ -1,11 +1,20 @@
 ﻿using System;
 using System.Text;
+using System.Text.RegularExpressions;
 
 namespace AdventOfCode2023 // Note: actual namespace depends on the project name.
 {
     internal class Program
     {
+        enum Numbers { zero = 0, one = 1, two = 2, three = 3, four = 4, five = 5, six = 6, seven = 7, eight = 8, nine = 9 }
+
         static void Main(string[] args)
+        {
+
+            //firstStar(); 
+            secondStar();
+        }
+        public static void firstStar()
         {
             Console.WriteLine("Enter your text (type 'END' to stop):");
             StringBuilder sb = new StringBuilder();
@@ -38,6 +47,63 @@ namespace AdventOfCode2023 // Note: actual namespace depends on the project name
                     if (int.TryParse(str, out int number))
                     {
                         totalSum += number;
+                    }
+                }
+
+                sb.AppendLine(line);
+            }
+
+            sb.AppendLine("Total sum of all numbers: " + totalSum);
+            Console.WriteLine("You entered:\n" + sb.ToString());
+        }
+        public static void secondStar()
+        {
+            //FUUUCK
+            Regex numberRegex = new Regex(@"\b(one|two|three|four|five|six|seven|eight|nine)\b");
+            Dictionary<string, int> numberDictionary = new Dictionary<string, int>()
+            {
+                { "one", 1 },
+                { "two", 2 },
+                { "three", 3 },
+                { "four", 4 },
+                { "five", 5 },
+                { "six", 6 },
+                { "seven", 7 },
+                { "eight", 8 },
+                { "nine", 9 }
+            };
+
+            Console.WriteLine("Enter your text (type 'END' to stop):");
+            StringBuilder sb = new StringBuilder();
+            int totalSum = 0;
+
+            while (true)
+            {
+                string line = Console.ReadLine();
+                if (line.ToUpper() == "END")
+                {
+                    break;
+                }
+
+                MatchCollection matches = numberRegex.Matches(line);
+                string firstNumber = null, lastNumber = null;
+
+                foreach (Match match in matches)
+                {
+                    if (firstNumber == null)
+                    {
+                        firstNumber = match.Value;
+                    }
+                    lastNumber = match.Value;
+                }
+
+                if (firstNumber != null)
+                {
+                    string str = firstNumber + (lastNumber ?? firstNumber);
+                    if (int.TryParse(str, out int concatenatedNumber))
+                    {
+                        totalSum += concatenatedNumber;
+                        Console.WriteLine($"{line} - {firstNumber} + {lastNumber ?? firstNumber} - {concatenatedNumber}");
                     }
                 }
 
