3da20ba1ea47b112a77f3ab684633c9319f1e1e1
Jan-P. R <janpatrickrotter@gmail.com>
2023-12-02 19:39:28 +0100

    Projektdateien hinzufügen.

---
+++ AdventOfCode2023/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Text;
+
+namespace AdventOfCode2023 // Note: actual namespace depends on the project name.
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter your text (type 'END' to stop):");
+            StringBuilder sb = new StringBuilder();
+            int totalSum = 0;
+
+            while (true)
+            {
+                string line = Console.ReadLine();
+                if (line.ToUpper() == "END")
+                {
+                    break;
+                }
+
+                char firstNumber = '\0', lastNumber = '\0';
+                foreach (char c in line)
+                {
+                    if (char.IsDigit(c))
+                    {
+                        if (firstNumber == '\0')
+                        {
+                            firstNumber = c;
+                        }
+                        lastNumber = c;
+                    }
+                }
+
+                if (firstNumber != '\0')
+                {
+                    string str = firstNumber.ToString() + lastNumber.ToString();
+                    if (int.TryParse(str, out int number))
+                    {
+                        totalSum += number;
+                    }
+                }
+
+                sb.AppendLine(line);
+            }
+
+            sb.AppendLine("Total sum of all numbers: " + totalSum);
+            Console.WriteLine("You entered:\n" + sb.ToString());
+        }
+
+    }
+
+}   
+    
